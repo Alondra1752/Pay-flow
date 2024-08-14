e8cbf06c66d9df0dded213e1d2873f39f571eca4
Alondra1752 <alondrasant7739@gmail.com>
2024-08-14 17:48:50 -0500

    added code to display employee data

--- Develop/script.js
+++ Develop/script.js
@@ -3,17 +3,66 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
 // Collect employee data
 const collectEmployees = function() {
+  let employees = [];
+  letaddEmployee = true;
+
   // TODO: Get user input to create and return an array of employee objects
+  while (addEmployee) {
+    const firstName = prompt("Enter employee's first name:");
+    const lastName = prompt("Enter employee's last name:");
+    const salaryInput = prompt("Enter employee's salary:");
+    const salary = isNaN(Number(salaryInput)) ? 0 : Number(salaryInput);
+    
+    const employee = {
+      firstName: firstName,
+      lastName: lastName,
+      salary: salary
+    };
+    
+    employees.push(employee);
+    addEmployee = confirm("Do you want to add another employee?");
+  }
+  return employees;
 }
+
+// Function to collect the employee data
+const employeeData = collectEmployees();
+// This will display the array of employee objects 
+console.log(employeeData); 
+
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
+  let totalSalary = 0;
+
+  // This will calculate the total salary of all employees
+  employeesArray.forEach(employee => {
+    totalSalary += employee.salary;
+  });
+  
   // TODO: Calculate and display the average salary
-}
+const averageSalary = totalSalary / employeesArray.length;
+
+// Log the average salary to the console
+console.log(`The average employee salary between our ${employeesArray.length} employee(s) is $${averageSalary.toFixed(2)}`);
+
+};
+
+const employees = [
+  {firstName: "John", lastName: "Doe", salary: 50000 },
+  {firstName: "Jane", lastName: "Smith", salary: 60000 },
+  {firstName: "Alice", lastName: "Johnson", salary: 70000},
+];
+
+displayAverageSalary(employees);
+
+
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
+
   // TODO: Select and display a random employee
+  
 }
 
 /*
